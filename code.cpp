bc8a4971a90f44e98d4b9d063b263f440dcadeb3
Yashwant Saiarjun.S.V <86396701+YashwantSaiarjun@users.noreply.github.com>
2022-06-03 11:36:47 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
 #include<stdio.h>
 using namespace std;
-void calculate(int n,int credits[],int marks[]);
+float calculate(int n,int credits[],int marks[]);
+void marksheet(int n,float cgpa,int marks[],int total);
 
-void calculate(int n,int credits[],int marks[])
+float calculate(int n,int credit[],int marks[])
 {
     int gp[n];
     float cgpa,total=0,total_credits=0;
@@ -23,40 +24,89 @@ void calculate(int n,int credits[],int marks[])
     
     for(int i=0;i<n;i++)
     {
-        total = total + (credits[i]*gp[i]);
-        total_credits = total_credits + credits[i];
-    }
+        total = total + (credit[i]*gp[i]);
+        total_credits = total_credits + credit[i];
+    } 
 
     cgpa = total/total_credits;
 
     cout<<"\nCGPA : "<<cgpa;
+    cout<<"\n---------------------------------------------------------------------------";
+    cout<<"\n---------------------------------------------------------------------------";
+    return cgpa;
+}
+
+
+void marksheet(int n,float cgpa,int marks[])
+{
+    
+    int rollno;
+    char name[20];
+    int total_marks=0;
+    for(int i=0;i<n;i++)
+    {
+        total_marks = total_marks + marks[i];
+    }
+    cout<<"\nENTER NAME : ";
+    cin>>name;
+    cout<<"\nENTER ROLLNO : ";
+    cin>>rollno;
+    
+    cout<<"\n---------------------------------------------------------------------------";
+    cout<<"\n---------------------------------------------------------------------------";
+    cout<<"\n|"<<" NAME : "<<name<<"               "<<"ROLLNO : "<<rollno<<"                                   |";
+    cout<<"\n----------------------------------------------------------------------------";
+    cout<<"\n|                                       |                                  |";
+    cout<<"\n|               SUBJECT                 |              MARKS               |";
+    cout<<"\n|                                       |                                  |";
+    cout<<"\n----------------------------------------------------------------------------"; 
+    for(int i=0;i<n;i++)
+    {
+        cout<<"\n|               SUBJECT "<<i+1<<"               |              "<<marks[i]<<"                  |";
+    }
+    cout<<"\n----------------------------------------------------------------------------"; 
+    cout<<"\n|               TOTAL                   |              "<<total_marks<<"                 |";
+    cout<<"\n----------------------------------------------------------------------------";    
 }
 
 int main()
 { 
-    int n,credit[15],marks[15],temp=0;
+    int n,credit[15],marks[15],temp=0,total;
+    float cgpa;
+    cout<<"\n---------------------------------------------------------------------------";
+    cout<<"\n\t\t\tGPA / CGPA CALCULATOR ";
+    cout<<"\n---------------------------------------------------------------------------";
     cout<<"\nENTER THE NUMBER OF SUBJECTS : ";
     cin>>n;
-    cout<<"\nENTER THE MARKS FOR EAH SUBJECT  :\n ";
+    cout<<"\nENTER THE MARKS FOR EACH SUBJECT  \n ";
     for(int i=0;i<n;i++)
     {
         cout<<"---------------------------------------------------------------------------";
         cout<<"\n\t\t\t\tSUBJECT "<<i+1;
         cout<<"\nMARKS : ";
         cin>>marks[i];
-        cout<<"\nCREDIT : ";
+        cout<<"CREDIT : ";
         cin>>credit[i];
         if(marks[i]<=49)
             temp=1;
     }
-
     if(temp==1)
     {
         cout<<"\nSorry you are not elgible to calculate cgpa"; 
+        cout<<"\nReason: You may have scored marks <=49 in any subject/subjects ";
     }
     else
     {
-        calculate(n,credit,marks);   
+        cgpa,total = calculate(n,credit,marks);   
     }
+    char ch;
+    cout<<"\nDO WANT THE MARKSHEET (y/n) : ";
+    cin>>ch;
+    if(ch=='y')
+    {
+        marksheet(n,cgpa,marks);
+    }
+
+
     return 0;
 }
